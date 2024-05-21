f6b62d68e42ba0f671cc4a58f0c5452a9d42ec3e
Mariem Moula <moula.meriame@gmail.com>
2024-05-21 11:34:36 +0100

    Initial commit

---
+++ src/main/java/com/mimilearning/springcoredemo/rest/DemoController.java
@@ -0,0 +1,33 @@
+package com.mimilearning.springcoredemo.rest;
+
+import com.mimilearning.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define a private field for the dependency
+    private Coach myCoach ;
+
+
+ //define a setter method and use   @Autowired
+    @Autowired
+    public void setMyCoach(Coach theCoach){
+        myCoach=theCoach;
+    }
+
+    //using the autowiring on a different method
+//    @Autowired
+//    public void soSomeStuff(Coach theCoach){
+//        myCoach=theCoach;
+//
+//    }
+
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+}
